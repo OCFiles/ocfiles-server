8cbffdb8506d17fe47362a4b8d8215c42d4c7e53
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-01-29 23:42:46 +0000

    create files modules routes

---
+++ routes/files/controller.js
@@ -0,0 +1,112 @@
+'use strict';
+
+const fs = require('fs');
+const mime = require('mime-types');
+const multer = require('multer');
+const path = require('path');
+const uuidV4 = require('uuid/v4');
+
+const config = require('../../config/enviroment');
+
+const uploadDir = path.join(config.root, './uploads');
+// Multer disk storage configuration
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${uuidV4()}${path.extname(file.originalname)}`);
+    }
+});
+// Instansiate the multer
+const upload = multer({
+    storage: storage
+});
+
+
+module.exports = function (gfs) {
+    return {
+        uploadFile(req, res) {
+            const tempFilePath = `${uploadDir}/${req.file.filename}`;
+            const writeStream = gfs.createWriteStream({
+                filename: req.file.filename,
+                content_type: mime.contentType(req.file.filename),
+                metadata: {
+                    mime: mime.lookup(path.extname(req.file.filename))
+                }
+            });
+            fs.createReadStream(tempFilePath)
+                .on('end', () => {
+                    fs.unlink(tempFilePath, (err) => {
+                        return res.status(201).send({
+                            ok: true,
+                            fileName: req.file.filename,
+                            filePath: `${config.host}/api/v1/files/${req.file.filename}`,
+                            message: 'File has been successfully created'
+                        });
+                    });
+                })
+                .on('error', (err) => {
+                    return res.status(500).send({
+                        ok: false,
+                        message: 'Error uploading file',
+                        error: err.message
+                    });
+                })
+                .pipe(writeStream);
+        },
+        checkIfFileExist(req, res, next) {
+            gfs.findOne({
+                filename: req.params.filename
+            }, function (err, file) {
+                if (err) {
+                    return res.status(500).send({
+                        ok: false,
+                        message: err.message
+                    });
+                }
+                if (!file) {
+                    return res.status(404).send({
+                        ok: false,
+                        message: 'File not found!'
+                    });
+                }
+                req.file = file;
+                next();
+            });
+        },
+        getFile(req, res) {
+            res.type(req.file.contentType);
+            const readStream = gfs.createReadStream({
+                filename: req.params.filename
+            });
+
+            readStream.on('error', (err) => {
+                return res.status(500).send({
+                    ok: false,
+                    message: 'File not found!',
+                    error: err.message
+                });
+            });
+            readStream.pipe(res);
+        },
+        deleteFile(req, res) {
+            gfs.remove({
+                filename: req.params.filename
+            }, function (err) {
+                if (err) {
+                    return res.status(500).send({
+                        ok: false,
+                        message: 'Something went wrong please try later',
+                        error: err.message
+                    });
+                }
+                return res.status(200).send({
+                    ok: true,
+                    message: `${req.params.filename} has been successfully deleted!`
+                });
+            });
+        },
+        upload
+    };
+};
---
+++ routes/files/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = function(gfs) {
+    const controller = require('./controller')(gfs);
+    // Files routes
+    router
+        .post('/', controller.upload.single('file'), controller.uploadFile)
+        .get('/:filename', controller.checkIfFileExist, controller.getFile)
+        .delete('/:filename', controller.checkIfFileExist, controller.deleteFile);
+
+    return router;
+};
