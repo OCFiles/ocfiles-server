38d20ac691074d3ff3887ec01d0ff8927d77208b
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2017-01-29 16:39:29 +0000

    Set config folder for different environments : construction:

---
+++ config/enviroment/development.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Development specific configuration
+module.exports = {
+    // MongoDB connection options
+    mongo: {
+        uri: process.env.MONGODB_URI || 'mongodb://localhost/own-files-cloud-dev'
+    },
+    seedDB: true
+};
